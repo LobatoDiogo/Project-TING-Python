097adcab4fffd17367c593b47084ac3bf5dadf08
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-13 14:52:59 -0300

    implemented a queue to store the files that will be read

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,26 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        pass
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
+
+    def size(self):
+        return len(self.queue)
+
+    def is_empty(self):
+        return self.size() == 0
