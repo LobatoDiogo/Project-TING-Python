f07abcbed6a3b147666d42aa059dc84fe43126b2
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-13 15:31:53 -0300

    implement function to import news from a txt file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    try:
+        with open(path_file, "r") as file:
+            files = [line.rstrip("\n") for line in file]
+
+        return files
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
