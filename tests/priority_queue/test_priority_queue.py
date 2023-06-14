9af8b425c99a652705bcbb6e8969cbc7b6d9bc8f
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-14 11:05:40 -0300

    tests

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,32 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+
+priority = {
+        "nome_do_arquivo": "arquivo_teste.txt",
+        "qtd_linhas": 1,
+        "linhas_do_arquivo": [],
+    }
+
+not_priority = {
+        "nome_do_arquivo": "arquivo_teste.txt",
+        "qtd_linhas": 9,
+        "linhas_do_arquivo": [],
+    }
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    queue.enqueue(not_priority)
+    queue.enqueue(priority)
+
+    assert len(queue) == 2
+    assert queue.search(0) == priority
+
+    result = queue.dequeue()
+    assert result == priority
+    assert len(queue) == 1
+
+    with pytest.raises(IndexError):
+        queue.search(3)
