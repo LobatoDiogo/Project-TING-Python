dd7a137427d4ae766dc19f72006f7dcb3c1d546b
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2023-06-14 12:05:35 -0300

    implemented search_by_word. Search a word in all processed files

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+
+    for index in range(len(instance)):
+        data = instance.search(index)
+        file = data["nome_do_arquivo"]
+        occurrences = [
+            {"linha": line_index, "conteudo": line}
+            for line_index, line
+            in enumerate(data["linhas_do_arquivo"], start=1)
+            if word.lower() in line.lower()
+        ]
+
+        if occurrences:
+            response.append({
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": occurrences
+            })
+
+    return response
